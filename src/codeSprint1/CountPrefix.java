1fb7825d8bda12eaa038d86d84e7a5a3fd96722a
Pravalika D <87607814+Pravalikanetha@users.noreply.github.com>
2025-01-09 22:04:21 +0530

    changes in code sprint 1

---
+++ src/codeSprint1/CountPrefix.java
@@ -0,0 +1,26 @@
+package codeSprint1;
+
+import java.util.HashMap;
+
+public class CountPrefix {
+	public static int prefixCount(String[] words, String pref) {
+		int count = 0 ; 
+		for (int i = 0; i < words.length; i++) {
+			if(words[i].startsWith(pref)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		String[] words = {"leetcode","win","loops","success"};
+		String pref = "at";
+//		System.out.println(prefixCount(words, pref));
+		HashMap<Integer,Integer> hm = new HashMap<>();
+		hm.put(2, 1);
+		hm.put(1, 2);
+		hm.put(3, 1);
+		hm.forEach((hello,hi)-> System.out.println(hello +":"+hi));
+	}
+}
