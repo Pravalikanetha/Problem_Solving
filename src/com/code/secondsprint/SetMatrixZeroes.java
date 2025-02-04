ad5fb0b6a75c0909776923b3e3690da99adbddc0
Pravalika D <87607814+Pravalikanetha@users.noreply.github.com>
2025-02-04 18:49:57 +0530

    changes

--- src/com/code/secondsprint/SetMatrixZeroes.java
+++ src/com/code/secondsprint/SetMatrixZeroes.java
@@ -41,7 +41,7 @@ public class SetMatrixZeroes {
 		}
 	}
 	public static void main(String[] args) {
-		int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+		int[][] matrix = {{0,1,0,0},{3,4,5,2},{1,3,1,5}};
 		setZeroes(matrix);
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
